b0b69a3639bb9a5c6aa3de6a80ac2c23d474115f
Vitalii Dubinin <vitaliidubinin@Vitaliis-MacBook-Pro.local>
2023-01-07 14:06:22 +0200

    sudo docker image prune -f deleted

---
+++ deploy_to_aws.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+echo 'Starting to Deploy...'
+ssh ubuntu@18.185.241.143 " cd todo/pern_todo_docker 
+        sudo docker-compose down
+        git fetch origin
+        git reset --hard origin/develop  &&  echo 'You are doing well'
+        sudo docker-compose build && sudo docker-compose up -d
+        "
+echo 'Deployment completed successfully'
