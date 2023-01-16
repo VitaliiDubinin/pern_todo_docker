2b6d02ce117ff822a33d2bb97a298d728ebbfc21
vitalii <veresk@siltatel.fi>
2023-01-16 22:03:59 +0000

    trying through dep_to_aws

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -3,10 +3,10 @@ echo 'Starting to Deploy...'
 ssh ubuntu@18.185.241.143 " 
         # sudo docker image prune -f
         cd todo/pern_todo_docker 
-        sudo docker-compose down
-        git fetch origin
-        git reset --hard origin  &&  echo 'You are doing well'
+#        sudo docker-compose down
+#        git fetch origin
+#        git reset --hard origin  &&  echo 'You are doing well'
         # sudo docker-compose build && sudo docker-compose up -d
-        sudo docker-compose up -d
+#        sudo docker-compose up -d
         "
 echo 'Deployment completed successfully!!!'
--- gitact.sh
+++ gitact.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'Starting to Deploy...'
 # sudo docker image prune -f
-cd UI_RSP
-# sudo docker-compose down
+#cd UI_RSP
+#sudo docker-compose down
 #  git fetch origin
 # git reset --hard origin  &&  echo 'You are doing well'
 # sudo docker-compose build && sudo docker-compose up -d
