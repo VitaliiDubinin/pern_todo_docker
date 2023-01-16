a22eaa5a1d5911d1d7b87eb9ab298fc55aff7e2b
vitalii <veresk@siltatel.fi>
2023-01-16 20:36:27 +0000

    gitact.sf moved

---
+++ gitact.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo 'Starting to Deploy...'
+# sudo docker image prune -f
+cd UI_RSP
+# sudo docker-compose down
+#  git fetch origin
+# git reset --hard origin  &&  echo 'You are doing well'
+# sudo docker-compose build && sudo docker-compose up -d
+# sudo docker-compose up -d
+echo 'Deployment completed successfully!!'
